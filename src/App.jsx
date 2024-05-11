737f70ce8834fdf2acaafcd28c300c82596910cb
Akem-Ben <akemini.ndaobong@gmail.com>
2024-05-11 21:43:25 +0100

    setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }
+])
+
+function App() {
+
+  return (
+    <>
+    <RouterProvider router={routes} />
+    </>
+  )
+}
+
+export default App
