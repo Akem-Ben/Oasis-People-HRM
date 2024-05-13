eccb57bc1e3d8f0d12c72f96ffaccb978f284627
Akem-Ben <akemini.ndaobong@gmail.com>
2024-05-13 11:55:16 +0100

    navbar design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
+import Come from './pages/Come';
+import Go from './pages/Go';
 import Navbar from './components/Navbar';
 
 
@@ -7,6 +9,30 @@ const routes = createBrowserRouter([
   {
     path: '/',
     element: <Navbar />
+  },
+  {
+    path: '/dashboard',
+    element: <Home />
+  },
+  {
+    path: '/employees',
+    element: <Come />
+  },
+  {
+    path: '/departments',
+    element: <Go />
+  },
+  {
+    path: '/attendance',
+    element: <Home />
+  },
+  {
+    path: '/leaves',
+    element: <Come />
+  },
+  {
+    path: '/settings',
+    element: <Go />
   },
 ])
 
