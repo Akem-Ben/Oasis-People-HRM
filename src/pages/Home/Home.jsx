c4510ce5f140816ffb61abc8a296f546f37e6ec6
Akem-Ben <akemini.ndaobong@gmail.com>
2024-05-20 13:23:21 +0100

    bug fixes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,19 @@
-import React, {useState} from 'react';
-import oasisPhoto from '../../assets/homepage/Oasis.jpeg';
+import React, { useState } from "react";
+import oasisPhoto from "../../assets/homepage/Oasis.jpeg";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import oasisLogo from '../../assets/homepage/oasis-logo.png';
+import oasisLogo from "../../assets/homepage/oasis-logo.png";
 import { MdOutlineWavingHand } from "react-icons/md";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
- const initialFormlikLoginValues = {
+  const initialFormlikLoginValues = {
     loginKey: "",
     password: "",
   };
-
 
   const loginFormik = useFormik({
     initialValues: initialFormlikLoginValues,
@@ -76,16 +74,30 @@ function Home() {
       <section className="w-full max-w-md md:max-w-lg lg:max-w-xl bg-white bg-opacity-90 p-6 rounded-lg shadow-lg flex items-center justify-center">
         <div className="w-full h-full flex flex-col justify-center items-center">
           <div className="flex w-full gap-2 items-center justify-center mb-6">
-            <img src={oasisLogo} alt="Oasis Logo" width="40" className="rounded-xl" />
-            <div className="font-bold text-2xl text-center font-lexend">Oasis People HRM</div>
+            <img
+              src={oasisLogo}
+              alt="Oasis Logo"
+              width="40"
+              className="rounded-xl"
+            />
+            <div className="font-bold text-2xl text-center font-lexend">
+              Oasis People HRM
+            </div>
           </div>
           <div>
-          <div className="font-bold text-2xl text-center flex gap-2 font-lexend">Welcome <MdOutlineWavingHand className="mt-1 text-[#A86232]" /></div>
+            <div className="font-bold text-2xl text-center flex gap-2 font-lexend">
+              Welcome <MdOutlineWavingHand className="mt-1 text-[#A86232]" />
+            </div>
           </div>
           <div>
-          <div className="text-sm text-center mt-4 flex gap-2 font-lexend font-thin">Please login here</div>
+            <div className="text-sm text-center mt-4 flex gap-2 font-lexend font-thin">
+              Please login here
+            </div>
           </div>
-          <form className="w-full sm:w-[90%] mt-4" onSubmit={loginFormik.handleSubmit}>
+          <form
+            className="w-full sm:w-[90%] mt-4"
+            onSubmit={loginFormik.handleSubmit}
+          >
             <div className="mb-4">
               <input
                 type="text"
@@ -94,7 +106,7 @@ function Home() {
                 className="border-2 border-gray-200 text-[#7152F3] rounded-xl w-full p-2 placeholder:text-[#7152F3] placeholder:text-xs"
                 value={loginFormik.values.loginKey}
                 onChange={loginFormik.handleChange}
-                placeholder='Email Address'
+                placeholder="Email Address"
               />
               {loginFormik.errors.loginKey && (
                 <div className="text-red-500 text-sm mt-1">
@@ -109,15 +121,17 @@ function Home() {
                 className="border-2 text-[#7152F3] border-gray-200 rounded-xl w-full p-2 placeholder:text-[#7152F3] placeholder:text-xs"
                 value={loginFormik.values.password}
                 onChange={loginFormik.handleChange}
-                placeholder='Password'
+                placeholder="Password"
               />
-              <div className='flex justify-between mt-2'>
-              {loginFormik.errors.password && (
-                <div className="text-red-500 text-sm mt-1">
-                  <em>{loginFormik.errors.password}</em>
+              <div className="flex justify-between mt-2">
+                {loginFormik.errors.password && (
+                  <div className="text-red-500 text-sm mt-1">
+                    <em>{loginFormik.errors.password}</em>
+                  </div>
+                )}
+                <div className="text-sm mt-1 mr-2 text-[#7152F3 hover:text-[#7152F3] hover:cursor-pointer">
+                  <em>Forgot Password?</em>
                 </div>
-              )}
-              <div className='text-sm mt-1 mr-2 text-[#7152F3 hover:text-[#7152F3] hover:cursor-pointer'><em>Forgot Password?</em></div>
               </div>
             </div>
             <button
