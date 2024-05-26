0f66c637c9e86dd9933b89f97e0024d1cecd92cd
lukwagoraymond <lukwagoraymond3@gmail.com>
2024-05-26 23:38:24 +0300

    Updated client-routing object

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Root from './pages/Root.jsx'; import Home from './pages/Home/Home.jsx';
 import DashboardPage from './pages/Dashboard/DashboardPage.jsx'; import EmployeesPage
   from "./pages/Employees/EmployeesPage.jsx"; import AttendancePage from "./pages/Attendance/AttendancePage.jsx";
-  import EmployeePage from "./pages/Employee/EmployeePage.jsx"; import EmployeeProfile
-  from "./components/EmployeeProfile/EmployeeProfile.jsx"; import NewEmployeePage
-  from "./pages/Employee/NewEmployeePage.jsx";
+  import EmployeePage from "./pages/Employee/EmployeePage.jsx"; import NewEmployeePage
+  from "./pages/Employee/NewEmployeePage.jsx"; import ErrorPage from "./pages/ErrorDefaultPage.jsx";
+  import EditEmployeePage from "./pages/Employee/EditEmployeePage.jsx";
 import { ThemeContextProvider } from './contexts/ThemeContext';
-import MockAPITest from "./components/MockAPITest.jsx";
-
+import { MockAPITest } from "./components/MockAPITest.jsx";
 
 
 const routes = createBrowserRouter([
@@ -15,7 +14,7 @@ const routes = createBrowserRouter([
     path: '/signin', element: <Home/>
   },
   {
-    path: '/', element: <Root />,
+    path: '/', element: <Root />, errorElement: <ErrorPage />,
     children: [
       {
         path: '/dashboard',
@@ -30,19 +29,21 @@ const routes = createBrowserRouter([
         element: <AttendancePage />
       },
       {
-        path: "employee/:employeeId",
+        path: "employee/:id",
         element: <EmployeePage />,
-        children: [
-          {
-            path: "employee/:employeeId/profile",
-            element: <EmployeeProfile />
-          }
-        ]
+        loader: async ({ params }) => {
+          return await fetch(`/api/user/employees/${params.id}`);
+        }
       },
       {
         path: "/add-employee",
         element: <NewEmployeePage />
       },
+      {
+        path: "/edit-employee/:id",
+        element: <EditEmployeePage />,
+      },
+
       {
         path: "/mock-api",
         element: <MockAPITest />
